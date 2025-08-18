73ab79e37020d5067bcb42fcacdb79579e8951f9
Koomball <135945730+Koomball@users.noreply.github.com>
2025-08-18 19:05:52 +1000

    Delete commands directory

--- commands/ping.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  code: `
-    Ping: \`$pingMS\` | Uptime: <t:$round[$math[$math[$getTimestamp-$uptime]/1000];0]:R>
-  `,
-  data: {
-    name: "ping",
-    description: "Get the bot's ping and uptime",
-  },
-};
