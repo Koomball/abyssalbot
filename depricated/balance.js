672798f2d53960e8eaee076565463bd27204b4c0
Koomball <135945730+Koomball@users.noreply.github.com>
2025-08-18 18:55:27 +1000

    updating to current before reforming

---
+++ depricated/balance.js
@@ -0,0 +1,16 @@
+module.exports = {
+  independent: true,
+  code: `
+      $addContainer[
+            $addTextDisplay[**Bone Balance**]
+            $addTextDisplay[*$getMemberVar[bones;$authorId;$guildId]*]
+        $addSeparator[Large;true]
+            $addTextDisplay[**Bank**]
+            $addTextDisplay[*$getMemberVar[bones_bank;$authorId;$guildId]*]
+    ]
+    `,
+  data: {
+    name: "bal",
+    description: "View your bone balance."
+  }
+};
