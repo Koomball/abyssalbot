318617ad0c02d009ccd3cb17f3aef4778858f72d
Koomball <135945730+Koomball@users.noreply.github.com>
2025-08-18 19:28:23 +1000

    Update command_template.js

--- src/command_template.js
+++ src/command_template.js
@@ -1,9 +1,21 @@
 module.exports = {
+    independent: true, // true = no sub-command from folder, false = sub-command from folder.
   code: `
     CODE HERE
   `,
   data: {
     name: "command_name",
     description: "command_description",
+
+    // delete below if options are not needed
+    options: [
+      {
+        name: "option_1",
+        description: "option_description",
+        type: 6, // USER
+        required: true
+      },
+    ]
+    // delete above if options are not needed
   },
 };
